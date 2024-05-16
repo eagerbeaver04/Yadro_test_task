3ae4d37505fb13566c809d6bd274fd44fa205e22
eagerbeaver04 <mike.markov04@gmail.com>
2024-05-16 19:46:00 +0300

    Bug fix with time checking

--- Event.h
+++ Event.h
@@ -11,4 +11,21 @@ struct Event
     {
         return Message::make_string(time.first, ":", time.second, " ", id, " ", name, " ", extra_info);
     }
+
+    bool is_time_less(const time1& lower_bound)
+    {
+        int time_minutes = 60 * time.first + time.second;
+        int lower_bound_minutes = 60 * lower_bound.first + lower_bound.second;
+        if(time_minutes < lower_bound_minutes)
+            return true;
+        return false;
+    }
+    bool is_time_bigger(const time1 &upper_bound)
+    {
+        int time_minutes = 60 * time.first + time.second;
+        int upper_bound_minutes = 60 * upper_bound.first + upper_bound.second;
+        if (time_minutes > upper_bound_minutes)
+            return true;
+        return false;
+    }
 };
--- Reader.cpp
+++ Reader.cpp
@@ -15,14 +15,30 @@ Computer_club Reader::read(const std::string& filename)
     int number_of_tables = std::atoi(line.c_str());
     std::getline(file, line);
     std::pair<time, time> opening_time = Parser::parse_time_line(line);
+    
     std::getline(file, line);
     int cost_per_hour = std::atoi(line.c_str());
 
     Computer_club club(number_of_tables, cost_per_hour, opening_time) ;
-
+    Event event;
     while (std::getline(file, line))
     {
-       std::cout <<  Parser::parse_action_line(line).serialize() << std::endl;
+        event =  Parser::parse_action_line(line);
+        std::cout << event.serialize() << std::endl;
+        
+        switch (event.id)
+        {
+        case 1:
+            if (event.is_time_less(opening_time.first) || event.is_time_bigger(opening_time.second))
+            {
+                std::cout << "NotOpenYet" << std::endl;
+                continue;
+            }
+         break;
+       
+        default:
+         break;
+        }
     }
 
     file.close();
