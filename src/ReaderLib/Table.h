348cd409db7871c150654d93a48007482684800c
eagerbeaver04 <mike.markov04@gmail.com>
2024-05-17 21:26:24 +0300

    Add Cmake and refactor structure of project

--- src/ReaderLib/Table.h
+++ src/ReaderLib/Table.h
@@ -10,14 +10,4 @@ struct Table
     Table() : is_available(true), name(), start_time() {}
     Table(bool flag, const std::string name1, Time time1) : 
         is_available(flag), name(name1), start_time(time1) {}
-    Table& operator = (const Table& table)
-    {
-        if(this != &table)
-        {
-            is_available = table.is_available;
-            name = table.name;
-            start_time = table.start_time;
-        }
-        return *this;
-    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "Reader.h"
+#include "ReaderLib/Reader.h"
 
 int main(int argc, char* argv[])
 {
     if(argc != 2)
     {
         std::cerr << "Incorrect input" << std::endl;
-        return 1;
+        return 0;
     }
     
     Reader::read(argv[1]);
